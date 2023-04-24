d779ac228594783c72701efdc129fec42000223a
Erarnitox <david@erarnitox.de>
2023-04-24 21:36:44 +0200

    stuff

---
+++ Debug/CMakeFiles/dropengine.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dropengine.dir/engine/Display.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/Display.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/GameObject.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/GameObject.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/Log.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/Log.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/Mesh.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/Mesh.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/Shader.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/Shader.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/Texture.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/Texture.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/Vector3.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/Vector3.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/engine.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/engine.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/backends/imgui_impl_sdl.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/backends/imgui_impl_sdl.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui_demo.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui_demo.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui_draw.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui_draw.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui_tables.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui_tables.cpp.o.d"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui_widgets.cpp.o"
+  "CMakeFiles/dropengine.dir/engine/imgui-1.82/imgui_widgets.cpp.o.d"
+  "libdropengine.pdb"
+  "libdropengine.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dropengine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Debug/CMakeFiles/dropengine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dropengine.
+# This may be replaced when dependencies are built.
